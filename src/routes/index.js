ec2a8781b75132dae95705dcf2735adcd3edf7ba
Alexandra Tseniklidou <atseniklidou@live.com>
2017-10-21 10:13:47 +0300

    quick fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/PageLayout/PageLayout'
 import VideoGamesRoute from './VideoGames'
+import BoardGamesRoute from './BoardGames'
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
 
@@ -9,7 +10,7 @@ export const createRoutes = (store) => ({
   component   : CoreLayout,
   indexRoute  : VideoGamesRoute(store),
   childRoutes : [
-    VideoGamesRoute(store),
+    BoardGamesRoute(store),
   ]
 })
 
